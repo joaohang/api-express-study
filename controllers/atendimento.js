a8dc8c3ed66e4881666307b59549164b9b87aa50
hang <joao.hang@zenvia.com>
2021-05-04 17:17:04 -0300

    Configuration routes

---
+++ controllers/atendimento.js
@@ -0,0 +1,8 @@
+module.exports = app => {
+    app.get('/atendimento', (req, res) => res.send('Você está na rota de atendimento'));
+
+    app.post('/atendimento', (req, res) => {
+        console.log(req.body);
+        res.send('Você está na rota de cadastrar um  atendimento');
+    });
+}
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const express = require('express');
+const customExpress =  require('./config/customExpress');
 
-const app = express();
-
+const app = customExpress();
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000 fera'));
 
-app.get('/atendimento', (req, res) => res.send('Você está na rota de atendimento'));
